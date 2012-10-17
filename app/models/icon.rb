1bd586a934bb626934fc713f593acbcd6b0a9a4b
sadjehwty <sadjehwty@gmail.com>
2012-10-17 20:30:42 +0200

    icon random in show

--- app/models/icon.rb
+++ app/models/icon.rb
@@ -8,6 +8,12 @@ class Icon < ActiveRecord::Base
     self.content_type = image.content_type.chomp
   end
   
+  def self.random
+    if (c = count) != 0
+      find(:first, :offset =>rand(c))
+    end
+  end
+  
   protected
   def right_size?
     unless self.file.nil?
@@ -18,7 +24,7 @@ class Icon < ActiveRecord::Base
         errors.add(:uploaded_data, "hight to high") if size.height > 128 # parametro da prendere da qualche altra parte
         errors.add(:uploaded_data, "invalid image type") unless ['image/png','image/gif','image/jpeg'].include? image.mime_type # parametro da prendere da qualche altra parte
       rescue
-        errors.add(:uploaded_data, "invalid image type")
+        errors.add(:uploaded_data, "invalid file type")
       end
     end
   end
