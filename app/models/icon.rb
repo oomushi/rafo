210c6138436f10a2f800f12ae3580b52148dcb26
sadjehwty <sadjehwty@gmail.com>
2012-10-17 21:10:44 +0200

    creazione sotto oggetto in icon

--- app/models/icon.rb
+++ app/models/icon.rb
@@ -5,8 +5,9 @@ class Icon < ActiveRecord::Base
   validate :right_size?
   
   def uploaded_data= image
-    self.file = image.read
-    self.content_type = image.content_type.chomp
+    self.build_blob({
+      :file=>image.read,
+      :content_type=>image.content_type.chomp })
   end
   
   def self.random
@@ -17,9 +18,9 @@ class Icon < ActiveRecord::Base
   
   protected
   def right_size?
-    unless self.file.nil?
+    unless self.blob.file.nil?
       begin
-        image=MiniMagick::Image.read self.file
+        image=MiniMagick::Image.read self.blob.file
         size=image.size
         errors.add(:uploaded_data, "width to high") if size.width > 128 # parametro da prendere da qualche altra parte
         errors.add(:uploaded_data, "hight to high") if size.height > 128 # parametro da prendere da qualche altra parte
