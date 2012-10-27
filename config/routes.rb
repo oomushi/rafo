d9776a406b2c235d2285d2a4a5c7d6664e80b11a
sadjehwty <sadjehwty@gmail.com>
2012-10-27 15:59:08 +0200

    cambiata home

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rafo::Application.routes.draw do
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
-  root :to => "quotes#index"
+  root :to => "images#index"
   resources :users
   resources :sessions
 end
