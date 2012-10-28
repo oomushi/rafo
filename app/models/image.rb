f972eae727a8acc076bc1f230af7efe616bce278
sadjehwty <sadjehwty@gmail.com>
2012-10-28 11:39:37 +0100

    aggiunto commento

--- app/models/image.rb
+++ app/models/image.rb
@@ -81,7 +81,7 @@ ly lx cx rx ry
     hash = Hash[hash.zip(a)]
     d = Magick::Draw.new do
       self.pointsize = 16
-      self.font_family = "Helvetica"
+      self.font_family = "Arial"
       self.font_weight = Magick::BoldWeight
       self.stroke = 'none'
     end
@@ -160,7 +160,7 @@ ly lx cx rx ry
     buff.composite! hash["ly"],offX-hash["ly"].columns-oX,(h2-hash["ly"].rows)/2, Magick::OverCompositeOp
     buff.composite! hash["ry"],w2-offD+oD,(h2-hash["ry"].rows)/2, Magick::OverCompositeOp
     buff.composite! hash["dy"],(w2-hash["dy"].columns)/2,h2-offB+oB, Magick::OverCompositeOp
-    buff.annotate d,w2,h2,offX,offY+16, @string.transform(request)
+    buff.annotate d,w2,h2,offX,offY+16, @string.transform(request) # TODO Non stampo i caratteri strani
     buff.to_blob
   end
 end
